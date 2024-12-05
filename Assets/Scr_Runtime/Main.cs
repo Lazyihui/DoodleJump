e08a5fb5f020fad85e344133300f0d70d027448d
lazyihui <chengbaozi6@qq.com>
2024-12-05 10:23:37 +0800

    ESTABLISH

---
+++ Assets/Scr_Runtime/Main.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+    void Awake() {
+        Debug.Log("Hello World");
+    }
+
+    void Update() {
+
+    }
+}
